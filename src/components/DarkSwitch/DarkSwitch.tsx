2718145bf748446faa190f7adea6347f6969bdff
Jayden H Nguyen <nguyenhuuduchuy2712@gmail.com>
2025-01-30 15:25:34 -0500

    :sparkles: Adding size prop and style for large size

--- src/components/DarkSwitch/DarkSwitch.tsx
+++ src/components/DarkSwitch/DarkSwitch.tsx
@@ -3,16 +3,16 @@ import LeftCloud from './assets/left_cloud.svg';
 import RightCloud from './assets/right_cloud.svg';
 import classes from './DarkSwitch.module.scss';
 
-export function DarkSwitch(): React.JSX.Element {
-  const [isChecked, setIsChecked] = React.useState(false);
-  
-  const handleChange = React.useCallback(() => {
-    setIsChecked((prev) => !prev);
-  }, []);
+type DarkSwitchProps = {
+  size?: 'small' | 'medium' | 'large';
+  isDark?: boolean;
+  onClick: () => void;
+};
 
+export function DarkSwitch({ onClick, size = 'medium', isDark = false }: DarkSwitchProps): React.JSX.Element {
   return (
-    <label className={classes['switch']}>
-      <input type="checkbox" checked={isChecked} onChange={handleChange} className={classes['checkbox']} />
+    <label className={classes[`switch-${size}`]}>
+      <input type="checkbox" checked={isDark} onChange={onClick} className={classes['checkbox']} />
       <span className={classes['slider']}>
         <img className={classes['left-cloud']} src={LeftCloud} alt={'left-cloud'} />
         <img className={classes['right-cloud']} src={RightCloud} alt={'right-cloud'} />
