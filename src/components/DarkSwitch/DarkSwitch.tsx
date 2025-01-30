54b3ad902e32b04b0c70cd2f8c408bd536ef7163
Jayden H Nguyen <nguyenhuuduchuy2712@gmail.com>
2025-01-29 23:36:19 -0500

    :hammer: Adding bundle config

---
+++ src/components/DarkSwitch/DarkSwitch.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+
+export function DarkSwitch(): React.ReactElement {
+
+    return (
+        <div>
+            <p>HELLLOOOOO</p>
+        </div>
+    );
+}
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// import React from 'react';
+//
+// type TestComponentProps = {
+//     textText?: string;
+// };
+//
+// export const JaydenSwitch: React.FC<TestComponentProps> = ({ textText }) => {
+//     return <div>{textText ?? 'Hello!'}</div>;
+// };
+
+export * from './components'
