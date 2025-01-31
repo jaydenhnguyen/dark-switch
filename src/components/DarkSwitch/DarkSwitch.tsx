d09eb7542e3a74475b41e8bda2a25458bd7e93ba
Jayden H Nguyen <nguyenhuuduchuy2712@gmail.com>
2025-01-30 21:42:04 -0500

    :memo: Add JS doc

--- src/components/DarkSwitch/DarkSwitch.tsx
+++ src/components/DarkSwitch/DarkSwitch.tsx
@@ -4,15 +4,41 @@ import RightCloud from './assets/right_cloud.svg';
 import classes from './DarkSwitch.module.scss';
 
 type DarkSwitchProps = {
+  /**
+   * The size of the switch.
+   * @default 'medium'
+   */
   size?: 'small' | 'medium' | 'large';
-  isDark?: boolean;
+  /**
+   * Whether the switch is in "dark mode" or not.
+   * @default false
+   */
+  isInDark?: boolean;
+
+  /**
+   * Callback function triggered when the switch is toggled.
+   */
   onClick: () => void;
 };
 
-export function DarkSwitch({ onClick, size = 'medium', isDark = false }: DarkSwitchProps): React.JSX.Element {
+/**
+ * The bring-me-the-designer switch that for toggling the light and dark mode.
+ *
+ * @example
+ * ```tsx
+ * <DarkSwitch size="large" isDark={true} onClick={() => setToggleDarkMode(!isDark)} />
+ * ```
+ * @version: 1.0.6
+ * @see: https://github.com/Jayjaynoplays/dark-switch
+ *
+ * @param onClick - Callback function triggered when the switch is toggled.
+ * @param size - The size of the switch. Default: `medium`
+ * @param isInDark - Whether the switch is in "dark mode" or not. Default: `false`
+ * */
+export function DarkSwitch({ onClick, size = 'medium', isInDark = false }: DarkSwitchProps): React.JSX.Element {
   return (
     <label className={classes[`switch-${size}`]}>
-      <input type="checkbox" checked={isDark} onChange={onClick} />
+      <input type="checkbox" checked={isInDark} onChange={onClick} />
       <span className={classes['slider']}>
         <img className={classes['left-cloud']} src={LeftCloud} alt={'left-cloud'} />
         <img className={classes['right-cloud']} src={RightCloud} alt={'right-cloud'} />
